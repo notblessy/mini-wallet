859640aa4488d2538af92c46dbabea8f3666ecb8
Frederich Blessy <frederichblessy@gmail.com>
2022-12-17 17:09:13 +0700

    init http mini wallet

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+	}))
+
+}
